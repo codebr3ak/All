fa24a5b81d2dfe86c94c2f69eebd2335d9244f4a
codebr3ak <barakaralph@gmail.com>
2021-09-29 21:17:50 +0300

    server now running

--- hello.py
+++ hello.py
@@ -1,10 +1,9 @@
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
 
-
-@app.route("/")
+@app.route('/')
 def index():
-    return '<h1>Hello world<h1>'
+    return '<h1> Hello World </h1>'
 
-if __name__ == '__main__'
-    app.run(debug = True) 
+if __name__ == '__main__':
+    app.run(debug = True)
