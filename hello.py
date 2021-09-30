e43014479d9e2232c55ddafc7f8fb21f226a1d7b
codebr3ak <barakaralph@gmail.com>
2021-09-30 16:38:10 +0300

    new routes

--- hello.py
+++ hello.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return '<h1> Hello World </h1>'
+def home():
+    return '<h1>This is the Home</h1>'
+
+@app.route('/')
+def about():
+    return '<h1>This is About page</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug = True)
